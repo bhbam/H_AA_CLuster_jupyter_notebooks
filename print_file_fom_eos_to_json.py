d9f88fabb38427ccab1d685de5f3bf3a33175d51
bhbam <bbbam@crimson.ua.edu>
2024-11-13 00:01:35 -0600

    mass pt plot for run 3 to check unbiasing

--- print_file_fom_eos_to_json.py
+++ print_file_fom_eos_to_json.py
@@ -1,24 +1,20 @@
 import glob, os
 import json
-# local="/eos/uscms/store/group/lpcml/bbbam/Ntuples/aToTauTau_Hadronic_tauDR0p4_m3p6To16_pT30To180_ctau0To3_eta0To1p4_pythia8_unbiased4ML_dataset_1/aToTauTau_Hadronic_tauDR0p4_m3p6To14_dataset_1_reunbaised_mannual_v2/230420_051900/0000"
-local="/eos/uscms/store/group/lpcml/bbbam/Ntuples_v2/signal/gen_HToAATo4Tau_Hadronic_tauDR0p4_M14_ctau0To3_eta0To2p4_pythia8_2018UL/HtoAATo4Tau_Hadronic_tauDR0p4_M14_eta0To2p4_pythia8_signal_v2/231013_053602/0000"
-rhFileList = '%s/output*.root'%(local)
-# local1="/eos/uscms//store/group/lpcml/bbbam/Ntuples_v2/aToTauTau_Hadronic_tauDR0p4_m14To17p2_pT30To180_ctau0To3_eta0To2p4_pythia8_dataset_2/aToTauTau_Hadronic_tauDR0p4_m14p8To17p2_eta0To2p4_pythia8_unbiased4ML_v2_dataset_2/230826_063114/0001"
-# rhFileList1 = '%s/output*.root'%(local1)
-# rhFileList = '%s/out*.root'%(local)
-# rhFileList = '%s/out*.root'%(local)
-rhFileList = glob.glob(rhFileList)
-# rhFileList1 = glob.glob(rhFileList1)
+import argparse
+parser = argparse.ArgumentParser(description="Collect and save sorted list of .root files in a directory to a JSON file.")
+parser.add_argument("--local", type=str,  default="/store/group/lpcml/bbbam/Ntuples_v2/signal/gen_HToAATo4Tau_Hadronic_tauDR0p4_M14_ctau0To3_eta0To2p4_pythia8_2018UL/HtoAATo4Tau_Hadronic_tauDR0p4_M14_eta0To2p4_pythia8_signal_v2/231013_053602/0000", help="Directory containing the .root files.")
+parser.add_argument("--output_json_file", type=str, default="signal_sample_M14.json", help="Name of the output JSON file.")
+args = parser.parse_args()
+
+local= args.local
+output_json_file = args.output_json_file
+rhFileList = '/eos/uscms%s/output*.root'%(local)
+rhFileList = sorted(glob.glob(rhFileList))
 assert len(rhFileList) > 0
 total_files = len(rhFileList)
-# assert len(rhFileList1) > 0
-# total_files1 = len(rhFileList1)
 print("Total file found :  ",total_files) #, "and  ", total_files1)
-# Define the output JSON file path
-List = rhFileList #+ rhFileList1
-
-output_json_file = "signal_sample_M14.json"
-
 # Write the filenames to the JSON file
+rhFileList = [path.replace("/eos/uscms", "root://cmseos.fnal.gov/") for path in rhFileList]
 with open(output_json_file, "w") as fout:
-    json.dump(List, fout)
+    json.dump(rhFileList, fout)
+print(f"output_json_file created: {output_json_file}")
