87fbb372562ab4008a545879b866d7d329a40c4d
bhbam <bbbam@crimson.ua.edu>
2025-10-06 12:32:59 -0500

    commit from LPC

--- print_file_fom_eos_to_json.py
+++ print_file_fom_eos_to_json.py
@@ -3,11 +3,11 @@ import json
 import argparse
 parser = argparse.ArgumentParser(description="Collect and save sorted list of .root files in a directory to a JSON file.")
 parser.add_argument("--local", type=str,  default="/store/group/lpcml/bbbam/Ntuples_v2/signal/gen_HToAATo4Tau_Hadronic_tauDR0p4_M14_ctau0To3_eta0To2p4_pythia8_2018UL/HtoAATo4Tau_Hadronic_tauDR0p4_M14_eta0To2p4_pythia8_signal_v2/231013_053602/0000", help="Directory containing the .root files.")
-parser.add_argument("--output_json_file", type=str, default="signal_sample_M14.json", help="Name of the output JSON file.")
+parser.add_argument("--output_json", type=str, default="signal_sample_M14.json", help="Name of the output JSON file.")
 args = parser.parse_args()
 
 local= args.local
-output_json_file = args.output_json_file
+output_json_file = args.output_json
 rhFileList = '/eos/uscms%s/output*.root'%(local)
 rhFileList = sorted(glob.glob(rhFileList))
 assert len(rhFileList) > 0
